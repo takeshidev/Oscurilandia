e7c3338c5dcd2f9531aa66a5b09d71b862f78d4c
Alumno <Alumno@DESKTOP-2I3AT3M>
2020-02-24 15:33:53 -0300

    commit 2

--- Oscurilandia_Combat/src/OscEjecutable.java
+++ Oscurilandia_Combat/src/OscEjecutable.java
@@ -4,6 +4,7 @@ public class OscEjecutable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola que tal");
+System.out.println("Chao");
 	}
 
 }
