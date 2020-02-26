ce5e68317d5f28c7eeb5db36f7f9d21870bd56ca
Alumno <Alumno@DESKTOP-LT0HL1R>
2020-02-26 18:52:54 -0300

    UML

--- Oscurilandia_Combat/src/juego/Ejecutable.java
+++ Oscurilandia_Combat/src/juego/Ejecutable.java
@@ -2,7 +2,7 @@ package juego;
 import java.util.Scanner;
 /**
  * Clase Main del programa, ejecuta el menu desde Clase Menu
- * @author Carlos Donoso, Takeshi Kita, Juan Carlos Tolorsa
+ * @author Carlos Donoso, Takeshi Kita, Juan Carlos Tolorza
  * @version 2.0.0
  */
 public class Ejecutable  {
